c9f6b1ebe06d6c44820434ca2a476a42cb366859
Lauren Watson <l.watson@ou.edu>
2020-01-21 13:00:34 -0600

    array method and calling it in main

--- DataAccess/Program.cs
+++ DataAccess/Program.cs
@@ -23,6 +23,21 @@ namespace DataAccess
             
             result = Subtract(num3, num4);
             Console.WriteLine($"{num3} - {num4} = {result}");
+          
+
+            //call array method
+
+            double[] allTheNumbers = new double[5];
+            allTheNumbers[0] = 1;
+            allTheNumbers[1] = 4;
+            allTheNumbers[2] = 5;
+            allTheNumbers[3] = 10;
+            allTheNumbers[4] = 20;
+            // allTheNumbers[5] = ERROR, INDEX OUT OF BOUNDS
+
+            result = Add(allTheNumbers);
+            Console.WriteLine(result);
+
             Console.ReadKey();
         }
        
@@ -38,6 +53,39 @@ namespace DataAccess
             double sum = val1 + val2;
             return sum;
         }
+
+        static double Add(double[] values)//square brackets mean array
+        {
+            //need a loop because they keep entering values, need to iterate through
+            //array or list, need a loop
+
+            double sum = 0;
+            int counter = 0; //need counting variable
+
+            //while
+            while (counter < values.Length)
+            {
+                sum += values[counter]; //+= same thing as saying sum = sum + values[counter]; keep adding values to itself
+                counter++; 
+            }
+
+            //for
+            sum = 0;
+            for (counter = 0; counter < values.Length; counter++) //tab twice fills it for you
+            {
+                sum += values[counter];
+            }
+
+            //foreach
+            sum = 0;
+            foreach (var item in values)
+            {
+                sum += item;
+            }
+
+            return sum; //overloading - two methods same name but work different, can do this becuase signatures & return values are different
+        }
+
         //subtraction
         public static double Subtract(double val1, double val2)
         {
