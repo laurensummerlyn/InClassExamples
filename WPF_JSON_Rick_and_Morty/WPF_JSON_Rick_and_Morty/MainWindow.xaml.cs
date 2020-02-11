b9219384b96564e87cae2ebd7290641aa944c9b8
Lauren Watson <l.watson@ou.edu>
2020-02-11 13:08:25 -0600

    character selection and image upload

---
+++ WPF_JSON_Rick_and_Morty/WPF_JSON_Rick_and_Morty/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_JSON_Rick_and_Morty
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// downloaded newton by right click on project, manage nuget package
+    /// Rick and MOrty json link; documentation to characters link and json formatting to get properties for classes
+    /// want list box to show characters 
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string apiURL = "https://rickandmortyapi.com/api/character/"; //link of all crazy unformatted json
+            RickAndMortyAPIResult apiInfo;
+            using (var client = new HttpClient())
+            {
+                string json = client.GetStringAsync(apiURL).Result;
+                //go to web server and wait until result comes back then give me result
+                //json variable will have all contents of the link
+
+
+                //need to convert json back to class
+                apiInfo = JsonConvert.DeserializeObject<RickAndMortyAPIResult>(json);
+
+            }
+
+            //results holds all character info; list object, need loop to iterate through
+            foreach (var character in apiInfo.results)
+            {
+                lstCharacters.Items.Add(character);
+            }
+        }
+
+        private void lstCharacters_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //harry potter choice on list box, select character to show image
+            ResultObject selectedCharacter = (ResultObject)lstCharacters.SelectedItem;
+            //give url to image; bitmap image downloads image bits
+            imgCharacter.Source = new BitmapImage(new System.Uri(selectedCharacter.image));
+        }
+    }
+}
---
+++ WPF_JSON_Rick_and_Morty/WPF_JSON_Rick_and_Morty/RickAndMortyAPIResult.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_JSON_Rick_and_Morty
+{
+    class RickAndMortyAPIResult
+    {
+        //only 2 there (in jsonformatter from r&m link) (info and results) so only need two properties
+        //info curly brackets on json formatter tells it's object
+        //square brackets on results tells array/collection, expand to see curly bracket meaning its list of object
+
+        public InfoObject info { get; set; } //infoobject and result object showed as errors, create class for each
+        public List<ResultObject> results { get; set; }
+    }
+
+    public class ResultObject
+    {
+        //result object has 12 properties, don't need to convert all (id name, gender, image, url (random selection))
+        public int id { get; set; }
+        public string name { get; set; }
+        public string status { get; set; }
+        public string species { get; set; }
+        public string gender { get; set; }
+        public string image { get; set; }
+        public override string ToString()
+        {
+            return $"({id}) {name}";
+        }
+
+    }
+
+    public class InfoObject
+    {
+        //info object has four properties
+        public int count { get; set; }
+        public int pages { get; set; }
+        public string next { get; set; }
+        public string prev { get; set; }
+
+    }
+}
